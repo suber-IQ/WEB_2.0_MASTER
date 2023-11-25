612f4661960af26bcfdbc48dc22139d24ce3a3f5
sumit <suberquercusumit@gmail.com>
2023-11-25 15:33:11 +0530

    amazon clone some issue fix and create some section

--- Mern/admin-dashboard/src/App.tsx
+++ Mern/admin-dashboard/src/App.tsx
@@ -1,13 +1,28 @@
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import { Suspense, lazy } from 'react';
+import Loader from './components/Loader';
 
+const Dashboard= lazy(() => import('./pages/Dashboard'));
+const Products= lazy(() => import('./pages/Products'));
+const Transaction = lazy(() => import('./pages/Transaction'));
+const Customers = lazy(() => import('./pages/Customers'));
 
 function App() {
 
   return (
     <Router>
-        <Routes>
-           <Route path='/admin/dashboard' element={} />
+       <Suspense fallback={<Loader />}>
+       <Routes>
+           <Route path='/admin/dashboard' element={<Dashboard />} />
+           <Route path='/admin/products' element={<Products />} />
+           <Route path='/admin/transaction' element={<Transaction />} />
+           <Route path='/admin/customers' element={<Customers />} />
+
+           {/* Charts */}
+           
+           {/* Apps */}
         </Routes>
+       </Suspense>
     </Router>
   )
 }

ea23110f37b75e3c513f29d354b91ef542f26ae0
sumit <suberquercusumit@gmail.com>
2023-12-01 22:44:58 +0530

    RealState website with completed navbar

---
+++ 50-Day-Challange-Html&Css/Real-Estate/assets/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// navbar toggle in mobile
+
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+
+
+// Header scroll state
+
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e => {
+ $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});

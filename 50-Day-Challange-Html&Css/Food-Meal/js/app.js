b74faf3a7b99456337617a6e1ed4412655945d3e
sumit <suberquercusumit@gmail.com>
2023-12-09 11:53:29 +0530

    completed food meal website with responsive

--- 50-Day-Challange-Html&Css/Food-Meal/js/app.js
+++ 50-Day-Challange-Html&Css/Food-Meal/js/app.js
@@ -4,6 +4,25 @@ $(document).ready(function() {
       slidesToShow: 3,
       slidesToScroll: 1,
       prevArrow: ".prev-btn",
-      nextArrow: ".next-btn"
+      nextArrow: ".next-btn",
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow: 2,
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 1,
+            }
+         }
+      ]
+   });
+
+   $('.nav-trigger').click(function() {
+      $('.site-content-wrapper').toggleClass('scaled');
    })
+
 });

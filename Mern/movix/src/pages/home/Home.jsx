ed3074fc3b87c4689ba243435e7c7841acf33815
sumit <suberquercusumit@gmail.com>
2023-12-12 10:29:17 +0530

    added hero section of Movix react app

---
+++ Mern/movix/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import './style.scss';
+import HeroBanner from './heroBanner/HeroBanner'
+const Home = () => {
+  return (
+    <div className="homePage">
+       <HeroBanner />
+    </div>
+  )
+}
+
+export default Home

fd6a4baa6d13d1b9dd95c6c0eaeb6ade2056ab2e
sumit <suberquercusumit@gmail.com>
2023-12-13 17:42:53 +0530

    added all movix react ott platform app fix all error

--- Mern/movix/src/utils/api.js
+++ Mern/movix/src/utils/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "http://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
+// const TMDB_KEY = import.meta.env.VITE_APP_TMDB_API_KEY;
 
 
 const headers = {
-      Authorization: 'bearer ' + TMDB_TOKEN,
+      Authorization: `Bearer ${TMDB_KEY}`,
 }
 
 export const fetchDataFromApi = async (url,params) => {
@@ -17,7 +18,7 @@ export const fetchDataFromApi = async (url,params) => {
        });
        return data;
    } catch (error) {
-      console.log(error);
+      // console.log(error);
       return error;
    }
 }

5f38d1f715905819b76106454a0a40d85bff093d
sumit <suberquercusumit@gmail.com>
2023-12-15 23:06:43 +0530

    added banner & learning section

--- 50-Day-Challange-Html&Css/udemy/app.js
+++ 50-Day-Challange-Html&Css/udemy/app.js
@@ -1,11 +1,38 @@
 function openNav(){
       document.getElementById("sideMenu").style.width = "50%";
       document.querySelector(".closebtn").style.display = "block";
-      
+
 }
 function closeNav(){
       document.getElementById("sideMenu").style.width = "0";
       document.querySelector(".closebtn").style.display = "none";
 }
 
-     
+$(document).ready(function(){
+      $('.learningSlider').slick({
+            autoplay: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: ".prev-btn",
+            nextArrow: ".next-btn",
+            responsive: [
+                  {
+                        breakpoint: 800,
+                        settings: {
+                           slidesToShow: 2,
+                        }
+                     },
+                     {
+                        breakpoint: 500,
+                        settings: {
+                           slidesToShow: 1,
+                        }
+                     },
+                  
+               ]
+
+      });
+});
+
+
+

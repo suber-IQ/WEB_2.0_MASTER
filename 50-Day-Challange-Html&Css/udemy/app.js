a359e1f38a493b22bbeac862de3e381302dfcd4d
sumit <suberquercusumit@gmail.com>
2023-12-15 18:33:29 +0530

    added header section of udemy clone website

---
+++ 50-Day-Challange-Html&Css/udemy/app.js
@@ -0,0 +1,11 @@
+function openNav(){
+      document.getElementById("sideMenu").style.width = "50%";
+      document.querySelector(".closebtn").style.display = "block";
+      
+}
+function closeNav(){
+      document.getElementById("sideMenu").style.width = "0";
+      document.querySelector(".closebtn").style.display = "none";
+}
+
+     

573f329fc6383e3b035a5807a2f3bd310f67ad7b
sumit <suberquercusumit@gmail.com>
2023-12-17 18:39:58 +0530

    added responsive udemy clone home page completed

--- 50-Day-Challange-Html&Css/udemy/app.js
+++ 50-Day-Challange-Html&Css/udemy/app.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
        autoplay: true,
        slidesToShow: 3,
        slidesToScroll: 1,
-       prevArrow: ".prev",
-       nextArrow: ".next",
+       prevArrow: ".prev-learning",
+       nextArrow: ".next-learning",
        responsive: [
           {
              breakpoint: 992,
@@ -33,5 +33,77 @@ $(document).ready(function() {
     // $('.nav-trigger').click(function() {
     //    $('.site-content-wrapper').toggleClass('scaled');
     // })
+ 
+ });
+$(document).ready(function() {
+    $('.item-slider').slick({
+       autoplay: true,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+       prevArrow: ".prev-item-icon",
+       nextArrow: ".next-item-icon",
+       responsive: [
+          {
+             breakpoint: 992,
+             settings: {
+                slidesToShow: 2,
+             }
+          },
+          {
+             breakpoint: 768,
+             settings: {
+                slidesToShow: 1,
+             }
+          }
+       ]
+    });
+ 
+ });
+$(document).ready(function() {
+    $('.popular-slider').slick({
+       autoplay: true,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+       prevArrow: ".popular-prev",
+       nextArrow: ".pupular-next",
+       responsive: [
+          {
+             breakpoint: 992,
+             settings: {
+                slidesToShow: 2,
+             }
+          },
+          {
+             breakpoint: 768,
+             settings: {
+                slidesToShow: 1,
+             }
+          }
+       ]
+    });
+ 
+ });
+$(document).ready(function() {
+    $('.featured-slider').slick({
+       autoplay: true,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+       prevArrow: ".featured-prev",
+       nextArrow: ".featured-next",
+       responsive: [
+          {
+             breakpoint: 992,
+             settings: {
+                slidesToShow: 2,
+             }
+          },
+          {
+             breakpoint: 768,
+             settings: {
+                slidesToShow: 1,
+             }
+          }
+       ]
+    });
  
  });

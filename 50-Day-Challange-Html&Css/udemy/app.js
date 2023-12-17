910cbefc335f5fd5b7ec1605ce0289f09a49f363
sumit <suberquercusumit@gmail.com>
2023-12-17 12:52:31 +0530

    added business section udemy clone

--- 50-Day-Challange-Html&Css/udemy/app.js
+++ 50-Day-Challange-Html&Css/udemy/app.js
@@ -7,32 +7,31 @@ function closeNav(){
       document.getElementById("sideMenu").style.width = "0";
       document.querySelector(".closebtn").style.display = "none";
 }
-
-$(document).ready(function(){
-      $('.learningSlider').slick({
-            autoplay: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            prevArrow: ".prev-btn",
-            nextArrow: ".next-btn",
-            responsive: [
-                  {
-                        breakpoint: 800,
-                        settings: {
-                           slidesToShow: 2,
-                        }
-                     },
-                     {
-                        breakpoint: 500,
-                        settings: {
-                           slidesToShow: 1,
-                        }
-                     },
-                  
-               ]
-
-      });
-});
-
-
-
+$(document).ready(function() {
+    $('.learning-row').slick({
+       autoplay: true,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+       prevArrow: ".prev",
+       nextArrow: ".next",
+       responsive: [
+          {
+             breakpoint: 992,
+             settings: {
+                slidesToShow: 2,
+             }
+          },
+          {
+             breakpoint: 768,
+             settings: {
+                slidesToShow: 1,
+             }
+          }
+       ]
+    });
+ 
+    // $('.nav-trigger').click(function() {
+    //    $('.site-content-wrapper').toggleClass('scaled');
+    // })
+ 
+ });

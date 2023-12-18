0726a8e8f2d3a95688b6defa557202b6afb83e5d
sumit <suberquercusumit@gmail.com>
2023-12-19 00:27:43 +0530

    added flipkart only  mobile responsive screen

---
+++ 50-Day-Challange-Html&Css/Flipkart/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+      $('.banner-slider').slick({
+            dots: true,
+            autoplay: true,
+            speed: 300,
+            slidesToShow: 1,
+            prevArrow: '.prev',
+            nextArrow: '.next',
+      });
+   
+   });

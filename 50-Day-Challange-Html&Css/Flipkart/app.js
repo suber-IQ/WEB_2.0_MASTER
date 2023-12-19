7e1349eceeabf8f779d0099f49d1ec04de8ed485
sumit <suberquercusumit@gmail.com>
2023-12-19 18:21:09 +0530

    added sidenav

--- 50-Day-Challange-Html&Css/Flipkart/app.js
+++ 50-Day-Challange-Html&Css/Flipkart/app.js
@@ -9,3 +9,41 @@ $(document).ready(function() {
       });
    
    });
+
+
+document.getElementById('main').addEventListener('click', closeNav);
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "60%";
+  document.getElementById("main").style.marginLeft = "60%";
+  document.getElementById("main").style.display = "block";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.display = "none";
+
+}
+
+
+// Get the search bar element
+const stickySearch = document.getElementById('stickySearch');
+
+// Get the offset position of the search bar
+const stickyOffset = stickySearch.offsetTop;
+
+// Function to add/remove sticky class based on scroll position
+function stickySearchBar() {
+  if (window.pageYOffset > stickyOffset) {
+    stickySearch.classList.add('sticky');
+  } else {
+    stickySearch.classList.remove('sticky');
+  }
+}
+
+// Event listener for scroll
+window.onscroll = function() {
+  stickySearchBar();
+};
